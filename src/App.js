85949f5af9e2cac5c02d5fd6feb516e7118f494e
Ignacio Damonte <nacho.damonte@hotmail.com>
2023-08-31 19:29:08 -0300

    commitprueba

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar/NavBar/NavBar.js"
-import ItemListContainer from "./components/NavBar/ItemListContainer/ItemListContainer.js";
-import ItemDetailContainer from "./components/NavBar/ItemDetailContainer/ItemDetailContainer.js";
-import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
+
+import { CartProvider } from "./context/CartContext";
+
 import "bulma/css/bulma.css";
+import "./App.css";
 
 function App() {
 
     return (
 
-      <div className="App">
-           <BrowserRouter>
-              <NavBar />
-              <Routes>
-                  <Route path="/" element={<ItemListContainer />} />
-                  <Route path="/category/:categoryId" element={<ItemListContainer />} />
-                  <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-                  <Route path="*" element={<h1>404 NOT FOUND</h1>} />
-              </Routes>
-            </BrowserRouter>
-            <footer className="futer" >
-                <p id="texto">Copyright Barsovia - 2023 . Todos los derechos reservados .</p>
-            </footer>
-      </div>
-    );
+        <div className="App">
+            <CartProvider>
+                <BrowserRouter>
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<ItemListContainer />} />
+                        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+                        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="/checkout" element={<Checkout />}/>
+                        <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+                    </Routes>
+                </BrowserRouter>
+                <footer className="has-background-black footer">
+                    <p className="texto">Copyright Compu SV   ●   Todos los derechos reservados   ●   Creado por Perez Jonatan. </p>
+                </footer>
+            </CartProvider>
+        </div>
+    )
 
 }
 
